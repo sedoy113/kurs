a628f96be55200d909b2912093cb88e2e9540e06
sedoy <C:\Users\sedoy\AppData\Roaming\The Bat!>
2018-04-18 20:18:45 +0300

    kurs

--- js/main.js
+++ js/main.js
@@ -3,32 +3,34 @@ let a = 24;
 console.log('Тест пройден, переменная =  ', a);
 /*end test*/
 //Diplom beginning
-window.addEventListener('DOMContentLoaded', function () {
 
-		let overlay = document.querySelector('.overlay');
-		let create = document.querySelector('#popup-btn');
-		let main = document.querySelector('.main');
-		let custom = document.querySelector('.custom');
-		let customPress = document.getElementsByClassName('custom-press');
+	//Modal Press
+	window.addEventListener('DOMContentLoaded', function () {
 
-		create.addEventListener('click', function() {
+			let overlay = document.querySelector('.overlay');
+			let create = document.querySelector('#popup-btn');
+			let main = document.querySelector('.main');
+			let custom = document.querySelector('.custom');
+			let customPress = document.getElementsByClassName('custom-press');
 
-			overlay.classList.add('animated', 'fadeOut');
-			setTimeout(function() {
-				overlay.style.display = 'none';
-			}, 1500);
-			main.classList.add('animated','fadeOut');
-			main.style.display = 'none';
-			custom.style.display = 'flex';
-			for (let i = 0; i < customPress.length; i++ ) {
-			showCustomPress.call(customPress[i])
-			};
+			create.addEventListener('click', function() {
 
-		});
+				overlay.classList.add('animated', 'fadeOut');
+				setTimeout(function() {
+					overlay.style.display = 'none';
+				}, 1500);
+				main.classList.add('animated','fadeOut');
+				main.style.display = 'none';
+				custom.style.display = 'flex';
+				for (let i = 0; i < customPress.length; i++ ) {
+				showCustomPress.call(customPress[i])
+				};
 
-	function showCustomPress() {
-		this.classList.add('animated','slideInUp');
-		this.style.display = 'block';
-	};
+			});
+
+		function showCustomPress() {
+			this.classList.add('animated','slideInUp');
+			this.style.display = 'block';
+		};
 
 })
