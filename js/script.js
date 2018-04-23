3d01d64a89e5142bbacc7bce9b830930352ba5af
sedoy <C:\Users\sedoy\AppData\Roaming\The Bat!>
2018-04-23 12:30:03 +0300

    kurs

--- js/script.js
+++ js/script.js
@@ -449,17 +449,17 @@ window.addEventListener('DOMContentLoaded', function () {
 	});
 
 		//накрутка под себя
-		let crimeBtn = document.getElementById('crime'),
-				countBlocks = document.getElementsByClassName('result-count'),
-				progressBlocks = document.getElementsByClassName('progress-bar'),
-				myCards = document.getElementsByClassName('main-cards-item');
+		let crimeBtn = document.getElementById('crime');
+		let countBlocks = document.getElementsByClassName('result-count');
+		let progressBlocks = document.getElementsByClassName('progress-bar');
+		let myCards = document.getElementsByClassName('main-cards-item');
 
 			crimeBtn.addEventListener('click', function() {
 
-				let first = 0.0,
-					second = 0.0,
-					third = 0.0,
-					total = 0.0;
+					let first = 0.0;
+					let second = 0.0;
+					let third = 0.0;
+					let total = 0.0;
 
 				first = rand();
 				second = rand();
@@ -541,50 +541,7 @@ window.addEventListener('DOMContentLoaded', function () {
 				progressBlocks[1].style.height = `${secondCandidate}%`;
 				progressBlocks[2].style.height = `${thirdCandidate}%`;
 			});
-/*		let crimeBtn = document.querySelector('#crime');
-		crimeBtn.addEventListener('click', function() {
 
-				let progressBar = document.querySelectorAll('.progress-bar');
-				let resultCount = document.querySelectorAll('.result-count');
-				let a = parseInt(progressBar[0].style.height);
-				let b = parseInt(progressBar[1].style.height);
-				let c = parseInt(progressBar[2].style.height);
-
-				let cheat;
-
-				if (a == 0 && b == 0) {
-				} else {
-				if (c >= 75) {
-				a = 0;
-				b = 0;
-				c = 100;
-				} else if(a == 0 && c < 75) {
-				a = a;
-				b = (b - 25);
-				c = (c + 25);
-			} else if(b == 0 && c < 75) {
-				a = (a - 25);
-				b = b;
-				c = (c + 25);
-			} else if (a == b && c < 75) {
-				a = (a - 12);
-				b = (b - 12);
-				c = (c + 25);
-			} else if(c < 75) {
-				a = (a - 25);
-				b = b;
-				c = (c + 25);
-						}; 
-				progressBar[0].style.height = `${a}%`;
-				progressBar[1].style.height = `${b}%`;
-				progressBar[2].style.height = `${c}%`;
-				resultCount[0].innerHTML = `${a}%`;
-				resultCount[1].innerHTML = `${b}%`;
-				resultCount[2].innerHTML = `${c}%`;
-				setTimeout(chooseWinner(a, b, c), 0);
-				}
-
-	});*/
 
 		//Сбросить результаты
 		
